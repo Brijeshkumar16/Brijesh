5d9acf6b988042754763b7784ab2099865524fd3
Brijesh <by8832866@gmail.com>
2021-01-02 11:14:26 +0530

    new

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
+// ===== TOGGLE BTN =====
 const btn = document.querySelector('nav .nav-center .logo i');
 const menu = document.querySelector('nav .nav-center .nav-link ul');
 btn.addEventListener('click',function(){
 menu.classList.toggle("active");
+});
+
+// ===== LOADER =====
+window.addEventListener("load", function () {
+    const loader = document.querySelector('.loader');
+    loader.classList.add('finish');
 });
