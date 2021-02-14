b5f40d2bd4d5555b609d45fa0d0a058145a96dc4
Brijesh <by8832866@gmail.com>
2021-02-14 17:14:34 +0530

    new

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 // ===== TOGGLE BTN =====
 const btn = document.querySelector('nav .nav-center .logo i');
 const menu = document.querySelector('nav .nav-center .nav-link ul');
-btn.addEventListener('click',function(){
-menu.classList.toggle("active");
+btn.addEventListener('click', function () {
+    menu.classList.toggle("active");
 });
 
-// ===== LOADER =====
-window.addEventListener("load", function () {
-    const loader = document.querySelector('.loader');
-    loader.classList.add('finish');
+// ===== CLOSE MENU IN MOBILE ON CLICK LINK =====
+const link = document.querySelectorAll('nav .nav-center .nav-link ul li a');
+link.forEach(function(item){
+item.addEventListener('click',function(){
+menu.classList.remove('active');
+});
 });
